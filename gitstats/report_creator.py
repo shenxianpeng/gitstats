4c95c27ab3dbb1e1c8b43bbfac0ce3bb06650174
shenxianpeng <xianpeng.shen@gmail.com>
2025-01-11 00:41:56 +0200

    fix code smells: initializes data and path

--- gitstats/report_creator.py
+++ gitstats/report_creator.py
@@ -19,7 +19,8 @@ class ReportCreator:
     """Creates the actual report based on given data."""
 
     def __init__(self):
-        pass
+        self.data = None
+        self.path = None
 
     def create(self, data, path):
         self.data = data
