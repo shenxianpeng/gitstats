f8a628d94650e7bf3e47f25a85169c153edca3d0
shenxianpeng <xianpeng.shen@gmail.com>
2025-03-29 07:51:11 +0800

    docs: update docstring

---
+++ noxfile.py
@@ -0,0 +1,53 @@
+import nox
+import shutil
+from pathlib import Path
+
+# Default tag for docker images
+TAG = "latest"
+
+
+@nox.session
+def image(session: nox.Session) -> None:
+    """Build docker image"""
+    session.run("docker", "build", "-t", f"gitstats:{TAG}", ".", external=True)
+
+
+@nox.session
+def publish_image(session: nox.Session) -> None:
+    """Publish docker image to ghcr"""
+    image(session)  # Build the image first
+    session.run(
+        "docker",
+        "tag",
+        f"gitstats:{TAG}",
+        f"ghcr.io/shenxianpeng/gitstats:{TAG}",
+        external=True,
+    )
+    session.run("docker", "push", f"ghcr.io/shenxianpeng/gitstats:{TAG}", external=True)
+
+
+@nox.session(name="install-deps")
+def install_deps(session: nox.Session) -> None:
+    """Install gnuplot"""
+    session.run("sudo", "apt", "update", "-y", external=True)
+    session.run("sudo", "apt", "install", "gnuplot", "-y", external=True)
+
+
+@nox.session
+def build(session: nox.Session) -> None:
+    """Generate gitstats report and json file"""
+    report_dir = Path("test-report")
+    if report_dir.exists():
+        shutil.rmtree(report_dir)
+    session.install("--upgrade", "pip")
+    session.install("-e", ".")
+    session.run("gitstats", ".", str(report_dir), "-f", "json", external=True)
+
+
+@nox.session
+def preview(session: nox.Session) -> None:
+    """Preview gitstats report in local"""
+    build(session)  # Generate report first
+    session.run(
+        "python3", "-m", "http.server", "8000", "-d", "test-report", external=True
+    )
