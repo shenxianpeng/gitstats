455c8560e0d3c5eb78239faad0c20ca27e9cc581
shenxianpeng <xianpeng.shen@gmail.com>
2026-03-10 23:58:44 +0200

    fix: update conf.py

--- docs/zh/conf.py
+++ docs/zh/conf.py
@@ -1,8 +1,18 @@
-import os
-import sys
+from datetime import datetime
 
-sys.path.insert(0, os.path.abspath("../source"))
-from conf import *  # noqa: F401, F403
+project = "GitStats"
+author = "Xianpeng Shen"
+copyright = (
+    f"2008 - {datetime.now().year}, Xianpeng Shen, Heikki Hokkainen, and contributors"
+)
+
+source_suffix = {
+    ".rst": "restructuredtext",
+}
+
+html_theme = "sphinx_rtd_theme"
+html_title = "gitstats"
+html_short_title = "gitstats"
 
 language = "zh"
 html_search_language = "zh"
